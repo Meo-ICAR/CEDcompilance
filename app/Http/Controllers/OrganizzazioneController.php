25c9add06b0b8df07e81e8a2346817e651bd4a80
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2025-07-18 14:38:26 +0200

    first commit

---
+++ app/Http/Controllers/OrganizzazioneController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Organizzazione;
+use Illuminate\Http\Request;
+
+class OrganizzazioneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Organizzazione $organizzazione)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Organizzazione $organizzazione)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Organizzazione $organizzazione)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Organizzazione $organizzazione)
+    {
+        //
+    }
+}
